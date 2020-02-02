ec114972aedf6f11e8eac14452f341a18c567341
kamranhossain <kamran.hossain227@gmail.com>
2020-02-03 00:24:54 +0600

    update main schema

--- morning_blog/schema.py
+++ morning_blog/schema.py
@@ -1,10 +1,14 @@
 import graphene
  
-class Query():
+from blog.schema.authors import schema as author_schema
+from blog.schema.blogs import schema as blog_schema
+ 
+ 
+class Query(author_schema.Query, blog_schema.Query):
     pass
  
  
-class Mutation():
+class Mutation(author_schema.Mutation, blog_schema.Mutation):
     pass
      
 schema = graphene.Schema(query=Query, mutation=Mutation)
