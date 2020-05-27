399d06ae6ef4b51bb53ef438267926ff75ac5b79
kamranhossain <kamran.hossain227@gmail.com>
2020-05-28 00:03:21 +0600

    add token_auth, verify and refresh token in mutation

--- backend/config/schema.py
+++ backend/config/schema.py
@@ -11,6 +11,9 @@ class Query(author_schema.Query, blog_schema.Query, user_schema.Query, graphene.
 
 
 class Mutation(author_schema.Mutation, blog_schema.Mutation, user_schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
