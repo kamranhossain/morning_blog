d2b51c5c2b0f45edd863e750a823addd454b0667
kamranhossain <kamran.hossain227@gmail.com>
2020-05-27 23:46:47 +0600

    add apps.users in base setting

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -41,7 +41,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = ["graphene_django"]
 
-LOCAL_APPS = ["apps.blog"]
+LOCAL_APPS = ["apps.blog", "apps.users"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
