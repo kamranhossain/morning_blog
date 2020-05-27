2192f4c35fffd27f047ce3c85043eb58a91d3032
kamranhossain <kamran.hossain227@gmail.com>
2020-05-27 23:46:17 +0600

    create createuser mutation

--- backend/apps/users/schema/users.py
+++ backend/apps/users/schema/users.py
@@ -24,3 +24,22 @@ class Query(graphene.ObjectType):
 
         return user
 
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
