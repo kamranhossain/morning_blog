03f59dd47f2a9444db0dd75260d040d158beaff4
kamranhossain <kamran.hossain227@gmail.com>
2020-05-27 23:45:35 +0600

    create user and me query with resolver

--- backend/apps/users/schema/users.py
+++ backend/apps/users/schema/users.py
@@ -9,3 +9,18 @@ class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
 
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(UserType, id=graphene.Int(required=True))
+    me = graphene.Field(UserType)
+
+    def resolve_user(self, info, id):
+        return get_user_model().objects.get(id=id)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError("Not logged in!")
+
+        return user
+
